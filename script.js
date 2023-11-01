16f1ab4380f469c4ca740251c19b7b21966c0d3d
alexramosiii-dev <alexramosiii.dev@gmail.com>
2023-11-01 12:18:45 +0800

    Added Buttons Functionality aside from gradual mode.

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
+let color_selector = document.querySelector("input[type=color]");
+let ncanvas_btn = document.querySelector("input[value=New]");
+let gradual_btn = document.querySelector("input[value=Gradual]");
+let colorful_btn = document.querySelector("input[value=Colorful]");
+let isColorful = false;
 
-let clr = document.querySelector("[type=color]");
+gradual_btn.addEventListener("click", e => {
+    e.target.classList.toggle("activated");
+}) 
+colorful_btn.addEventListener("click", e => {
+    e.target.classList.toggle("activated");
+    isColorful = !isColorful;
+}) 
+ncanvas_btn.addEventListener("click", () => 
+addTilesEvents(generateGrid())
+);
+
+//tracks if a mouse button is press hold.
+let mouseDown = 0;
+document.body.onmousedown = e=> {
+    if (e.button == 0) {
+        mouseDown = 1;
+    }else if (e.button == 2){
+        mouseDown = 2;
+    }
+}
+document.body.onmouseup = function() {
+    mouseDown = 0;
+}
+
+//add diff events in each tiles.
 
 
 
-function generateGrid(tileSize) {
+function generateGrid() {
+    let tileSize = parseInt(prompt());
     let cnv = document.querySelector(".canvas");
+    let tiles = [];
+    //clear canvas
     while (cnv.firstChild) {
         cnv.removeChild(cnv.firstChild);
     }
@@ -21,41 +53,69 @@ function generateGrid(tileSize) {
             let tile = document.createElement("div");
             tile.style.height = pixel_size + "px";
             tile.style.width = pixel_size + "px";
+            tile.style.backgroundColor = "white";
             tile.classList.add("tile");
-
-            tile.addEventListener("mouseover", e=>{
-                if(mouseDown){
-                    e.target.style.backgroundColor = clr.value;
-                }
-            })
-
-            tile.addEventListener("click", e=>{
-                e.target.style.backgroundColor = "#FFFFFF";
-
-            })
+            tile.draggable = false;
 
             fragment.appendChild(tile);
         }
+        tiles = tiles.concat(Array.from(fragment.childNodes));
+
         row.appendChild(fragment);
         cnv.appendChild(row);
     }
+    return tiles;
+}
+
+function paint(e) {
     
 }
 
-
-let btn_canvas = document.querySelector("input[value=New]");
-
-btn_canvas.addEventListener("click", () => {
-    let size = parseInt(prompt());
-
-    generateGrid(size)
-});
-
-let mouseDown = 0;
-document.body.onmousedown = function() { 
-    mouseDown = 1;
+function paintRandom(e){
+    let randomValue = () => (Math.floor(Math.random() * 255) + 1).toString(16);
+    let r = randomValue();
+    let g = randomValue();
+    let b = randomValue();
+    e.target.style.backgroundColor = "#"+r+g+b;
 }
-document.body.onmouseup = function() {
-    mouseDown = 0;
+
+function addTilesEvents(tiles) {
+    tiles.forEach(n => {
+        n.addEventListener("mousedown", (e) => {
+            if (e.button == 0 && isColorful){
+                paintRandom(e);
+            }
+            else if(e.button == 0) {
+                e.target.style.backgroundColor = color_selector.value;
+            }
+            else if (e.button == 2){
+                e.target.style.backgroundColor = "#FFFFFF";
+            }
+        });
+    
+        n.addEventListener("mouseover", e => {
+            if(mouseDown == 1 && isColorful) {
+                paintRandom(e);
+            }else if(mouseDown == 1) {
+               e.target.style.backgroundColor = color_selector.value;
+            }else if (mouseDown == 2) {
+                e.target.style.backgroundColor = "#FFFFFF";
+            }
+        });
+    
+       
+
+        //default preventers
+        n.addEventListener("contextmenu", e=> {
+            e.preventDefault();
+        });
+        n.addEventListener('dragstart', e => {
+            e.preventDefault();
+        });
+        n.addEventListener('drop', e => {
+            e.preventDefault();
+        });
+    })
 }
+
 
